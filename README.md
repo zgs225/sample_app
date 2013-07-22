b352811b9431a92a8ed586f6051aa86f167e8954
zgs225 <zgs225@gmail.com>
2013-07-22 21:19:41 +0800

    Initilize

---
+++ README.md
@@ -0,0 +1,6 @@
+Ruby On Rails tutorials
+=======================
+
+This is the sample application for
+the [*Ruby on Rails Tutorial*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2cf9b9b61f7701227e0f69977f6f3bc84e84661cee75bddba97a1622c5b29f3e205507c6d449765ae004f6558ee8e29496dc9fe754c59190a46e9daf7f38d724'
