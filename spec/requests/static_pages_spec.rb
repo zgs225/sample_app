90d919a7e78307ce4781a974efee4c5f49c6297a
zgs225 <zgs225@gmail.com>
2013-07-25 06:51:14 +0800

    Refactor rspec/requset

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,54 +1,36 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
+  subject { page }
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content("Sample App")
-    end
+    before { visit root_path }
 
-    it "should have the right title" do
-      visit root_path
-      expect(page).to have_title("#{ base_title } | Home")
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title(full_title('Home')) }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content("Help")
-    end
+    before { visit help_path }
 
-    it "should have the right title" do
-      visit help_path
-      expect(page).to have_title("#{ base_title } | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-    it "should have the content 'About'" do
-      visit about_path
-      expect(page).to have_content("About")
-    end
+    before { visit about_path }
 
-    it "should have the right title" do
-      visit about_path
-      expect(page).to have_title("#{ base_title } | About")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About'))}
   end
 
     describe "Contact page" do
-      it "shoud have the content 'Contact" do
-        visit contact_path
-        expect(page).to have_content("Contact")
-      end
+      before { visit contact_path }
 
-      it "shoud have the right title" do
-        visit contact_path
-        expect(page).to have_title("#{base_title } | Contact")
-      end
+      it { should have_content('Contact') }
+      it { should have_title(full_title('Contact')) }
     end
 
 end
