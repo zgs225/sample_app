c000ac3189609c045ab2380744b6b1259bc8aff0
zgs225 <zgs225@gmail.com>
2013-07-25 06:24:29 +0800

    config/route.rb modified, set the path for per_page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,48 +5,48 @@ describe "Static pages" do
 
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content("Sample App")
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{ base_title } | Home")
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content("Help")
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{ base_title } | Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content("About")
+    end
+
+    it "should have the right title" do
+      visit about_path
+      expect(page).to have_title("#{ base_title } | About")
     end
   end
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{ base_title } | About")
-    end
-
     describe "Contact page" do
       it "shoud have the content 'Contact" do
-        visit '/static_pages/contact'
+        visit contact_path
         expect(page).to have_content("Contact")
       end
 
       it "shoud have the right title" do
-        visit '/static_pages/contact'
+        visit contact_path
         expect(page).to have_title("#{base_title } | Contact")
       end
     end
