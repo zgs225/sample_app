8e374281b2effbf5ea5d3c244ff2ad0b454aa650
yuezz <843097013@qq.com>
2013-07-25 09:41:51 +0800

    Use shared_examples_for refactor rspec test examples

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,46 @@ describe "Static pages" do
   
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title(full_title('Home')) }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About'))}
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
+    
+    it_should_behave_like "all static pages"
   end
 
     describe "Contact page" do
       before { visit contact_path }
 
-      it { should have_content('Contact') }
-      it { should have_title(full_title('Contact')) }
+      let(:heading) { 'Contact' }
+      let(:page_title) { 'Contact' }
+
+      it_should_behave_like "all static pages"
     end
 
 end
