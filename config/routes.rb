adc1879dab41db1e09da43e68056ca04e299a2a8
zgs225 <zgs225@gmail.com>
2013-07-25 22:03:13 +0800

    code 7.15

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-  get "user/new"
+  resources :users
   root  :to => 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
