4bac267a27a7357ca3b1e3ab185667a124a68200
zgs225 <zgs225@gmail.com>
2013-07-27 10:49:00 +0800

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Force all access to the app Over SSL, use Strict-Transport-Security
+  # and use the secure cookies
+  config.force_ssl = true
 end
