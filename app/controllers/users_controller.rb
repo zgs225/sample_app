bd507c530d924636187b54e5e7e8c7e03af92066
zgs225 <zgs225@gmail.com>
2013-07-28 20:49:08 +0800

    By update with invalid inforamtion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by(params[:id])
+  end
+
+  def update
+    @user = User.find_by(params[:id])
+    if @user.update_attributes(user_params)
+      # Handler a successful update
+    else
+      render 'edit'
+    end
+  end
+
   private 
 
     def user_params
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -18,10 +18,17 @@ RSpec::Matchers.define :create_a_user do
   expect { click_button "Create my account" }.to change(User, :count).by(1)
 end
 
-def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
+def sign_in(user, options={})
+  if options[:no_capybara]
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
 end
 
 def fill_in_valid_information
