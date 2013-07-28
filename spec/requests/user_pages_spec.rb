11f8f1060197a1327d30eafbdc6bec47c09c78d3
zgs225 <zgs225@gmail.com>
2013-07-28 16:41:38 +0800

    Finish sign out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,8 +52,9 @@ describe "User Pages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success'), text: 'welcome' }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
