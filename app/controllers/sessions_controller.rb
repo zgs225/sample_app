bf6c5c06a968568804ab3a0a83161e680fc3ed30
yuezz <843097013@qq.com>
2013-07-29 10:52:48 +0800

    Finish friendly redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase) # Get use
+    user = User.find_by(email: params[:session][:email].downcase) # Get user
 
     if(user && user.authenticate(params[:session][:password]))
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination' 
       render 'new'
