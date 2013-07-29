e2b7d2ecf3e3235c926039066dff8ebc13e97088
zgs225 <zgs225@gmail.com>
2013-07-29 20:03:02 +0800

    Finish pagination

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -102,10 +102,18 @@ describe "User Pages" do
 
     it { should have_title('All users') }
     it { should have_content('All users') }
-    
-    it "should list each user" do
-      User.all.each do |user|
-        expect(page).to have_selector('li', text: user.name)
+
+    describe "pagenation" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
       end
     end
   end
