ebda8ff270e0246314b850b1dd203022d1ab3429
zgs225 <zgs225@gmail.com>
2013-07-30 20:56:49 +0800

    Finish post microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,13 +56,6 @@ class UsersController < ApplicationController
                                    :password_confirmation, :admin)
     end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in"
-      end
-    end
-    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
